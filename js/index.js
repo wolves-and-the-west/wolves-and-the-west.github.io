28a889322b3eb0a9e7698e73c6ef45447ed7004a
Chris Canipe <chris.canipe@kyrus-tech.com>
2017-02-04 12:21:13 -0700

    Prune.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,4 @@
-var NO_DATA = 'N/A';
 var load = function () {
-  // new Population(data);
-  // new PopulationTable(data);
   ['Colorado', 'Idaho', 'Montana', 'Oregon', 'Washington', 'Wyoming'].forEach(function(state, i) {
     new LineGraph(state, i == 0);
   });
